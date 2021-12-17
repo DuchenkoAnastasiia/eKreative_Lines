883816b4dc7442dab24e39439c753061d7d6e20f
Anastasiia Duchenko <nduchenko2001@gmail.com>
2021-12-17 17:06:21 +0200

    first commit

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 (function () {
-    const table = document.createDocumentFragment();
+  const table = document.createDocumentFragment();
+  const size = 11;
 
-    let arr = [];
-    for (let i = 0; i < 11; i++) {
-        const tr = document.createElement('tr');
-        arr[i] = [];
-        for(let j = 0; j < 11; j++) {
-            const td = document.createElement('td');
-            td.innerHTML = arr[i][j] = getRandom();
-            tr.appendChild(td);
-            if(td.innerHTML == 0){
-               td.style.backgroundColor = '#94f7d3';
-            }
+  let arr = [];
+  for (let i = 0; i < size; i++) {
+    const tr = document.createElement("tr");
+    arr[i] = [];
+    for (let j = 0; j < size; j++) {
+      const td = document.createElement("td");
+      td.innerHTML = arr[i][j] = getRandom();
 
-        }
-        table.appendChild(tr);
+      tr.appendChild(td);
+
+      if (td.innerHTML == 0) {
+        td.style.backgroundColor = "#94f7d3";
+      }
     }
-    document.getElementById('matrix').appendChild(table);
+    table.appendChild(tr);
+  }
+  document.getElementById("matrix").appendChild(table);
 
-    function getRandom(){
-        const min = 0;
-        const max = 2;
-        return Math.floor(Math.random()*(max-min))+min;
-    }
-    
+  const startRow = Math.round(Math.random() * size);
+  const endRow = Math.round(Math.random() * size);
+  const startCol = Math.round(Math.random() * size);
+  const endCol = Math.round(Math.random() * size);
+
+  console.log(`StartPoint: [${startRow},${startCol}]`);
+  console.log(`EndPoint: [${endRow},${endCol}]`);
+
+  function getRandom() {
+    const min = 0;
+    const max = 2;
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 })();
